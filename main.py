a365edd61baa181163e05d4d94f291b1b66a4754
nathanromanelli <nroma009@ucr.edu>
2022-10-31 14:39:26 -0700

    Working distance heuristic

--- main.py
+++ main.py
@@ -10,19 +10,24 @@ def missing_heuristic(state):
                 dist = dist + 1
     return dist
 
-def manhat_heuristic(state):
+def manhat_heuristic(state, h_n):
     dist = 0
     dimension = len(state[0])
-    for i in range(len(state)):
-        for j in range(len(state[i])):
+    for i in range(dimension):
+        for j in range(dimension):
             if (state[i][j] != dimension*i + j + 1):
-                #f(x,y) = dim * y + x, in the given example dimension = 3
-                dist = dist + 1
-                #Adding g(n)
-                goal_i = state[i][j] % dimension # f(x,y) % dim = x 
-                goal_j = state[i][j] - goal_i * dimension
-                dist = dist
-                #Adding h(n)
+                # f(x,y) = dim * y + x, in the given example dimension = 3
+                # Adding g(n)
+                # dist = dist + 1
+                
+                # if we're using manhattan, h_n is True, else we just use missing squares
+                # we don't care about the distance of the empty tile
+                if (state[i][j] != pow(dimension,2) and h_n) : 
+                    goal_j = (state[i][j] - 1) % dimension # f(x,y) % dim = x 
+                    goal_i = (state[i][j] - 1) // dimension # floor[f(x,y) / dim] = y
+
+                    # Adding h(n) and not counting blank square, which im leaving as 9 for simplicity
+                    dist = dist + abs(i - goal_i) + abs(j - goal_j)
     return dist
 
 
@@ -45,20 +50,16 @@ def astar_search(problem,queing_function):
 
 
 goal_state = [[1,2,3],[4,5,6],[7,8,9]]
-#goal_state = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
 test1 = [[2,3,4],[5,6,7],[8,9,1]]
 test2 = [[16,2,3,4],[5,6,7,8],[9,10,11,12],[13,1,15,14]]
-test3 = [[1,2,4],[3,0,6],[7,8,5]]
+test3 = [[1,2,4],[3,9,6],[7,8,5]]
+test4 = [[8,2,3],[4,5,6],[7,1,9]]
 
-print(missing_heuristic(test1))
-print(missing_heuristic(test2))
-print(missing_heuristic(test3))
-print(missing_heuristic(goal_state))
-
-print(manhat_heuristic(test1))
-print(manhat_heuristic(test2))
-print(manhat_heuristic(test3))
-print(manhat_heuristic(goal_state))
+print(manhat_heuristic(test1,h_n=True))
+print(manhat_heuristic(test2,h_n=True))
+print(manhat_heuristic(test3,h_n=True))
+print(manhat_heuristic(test4,h_n=True))
+print(manhat_heuristic(goal_state,h_n=True))
 
 #n = input("Puzzle dimension: ")
 
@@ -67,6 +68,6 @@ temp = []
 for i in range(4):
     for j in range(4):
         some = i*4 + j + 1
-        temp.append(some % 3)
+        temp.append((some-1)%4)
     print(temp)
     temp = []
